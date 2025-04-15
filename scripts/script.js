72979b3d6b028eca59d5736992d98d058e661cba
Ivana Velickovic <ivanavelickovic3101@gmail.com>
2025-04-15 13:09:44 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-import { data } from 'data.js'
+import { data } from './data.js'
 const listaKategorija = document.getElementById("navigation");
 const proizvodi = document.getElementById("proizvodi");
 let naslov = document.getElementById("naslov");
